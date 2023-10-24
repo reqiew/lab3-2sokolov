fe701d0604bbbb39449c7cf2d8749071ebb28255
dobry <dobry@DESKTOP-FBD3PAQ>
2023-10-24 16:02:37 +0300

    Добавьте файлы проекта.

---
+++ lab3-2sokolov/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.WriteLine("Введите накопления студента");
+double s = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите стипендию студента");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите траты студента");
+double b = double.Parse(Console.ReadLine());
+double c = 0;
+int v = 0;
+for (double i = 0; c >= 0; i++)
+{
+    c = (s + a) - b;
+    b += b / 100 * 3;
+    s -= b - a;
+    v++;
+}
+Console.WriteLine($"Студент выживет {v} месяцев");
